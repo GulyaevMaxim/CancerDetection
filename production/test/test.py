d1deb6f15a1a177ebc5290ea2b988d6988f8282f
Maxim Gulyaev <gulyaev@cvisionlab.com>
2019-07-29 14:15:34 +0300

    Added tests of deployment

---
+++ production/test/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import torch
+import sys
+import os
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import copy
+import json
+import argparse
+import torch.nn.functional as F
+import torchvision.transforms as T
+import torchvision
+from torch.utils.data import DataLoader
+from tensorboardX import SummaryWriter
+from tqdm import tqdm
+import albumentations
+from albumentations import torch as AT
+
+
+def main():
+    print('Installed')
+	
+if __name__ == '__main__':
+    main()
